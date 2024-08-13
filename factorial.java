a91cf88ff9576be5183d721e38a9f5a627cbb865
gundvinu1 <141437769+gundvinu1@users.noreply.github.com>
2024-08-13 15:37:35 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class factorial
+{
+public static void main(String[] args)
+{ 
+int n,i,prod=1;
+n=9;
+for (i = 1; i <= n; i++);
+{
+prod=prod*i;
+}
+System.out.println(prod);
+}
+}
